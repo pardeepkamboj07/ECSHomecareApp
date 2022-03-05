f1addb273ae5fb0dd0173d6d094fd8ee3572e4d6
Rakesh <rakeshup94@gmail.com>
2022-03-05 11:16:47 +0530

    update

--- src/app/app-bootstrap/app-bootstrap.module.ts
+++ src/app/app-bootstrap/app-bootstrap.module.ts
@@ -4,13 +4,9 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { CollapseModule} from 'ngx-bootstrap/collapse';
-<<<<<<< HEAD
-import { TabsModule,TabsetConfig } from 'ngx-bootstrap/tabs';
-=======
 
 
 
->>>>>>> fa55f335617a561697966b07e9ceab36d017fc64
 @NgModule({
   declarations: [],
   imports: [
@@ -19,11 +15,6 @@ import { TabsModule,TabsetConfig } from 'ngx-bootstrap/tabs';
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
     CollapseModule.forRoot(),
-<<<<<<< HEAD
-    TabsModule.forRoot()
-=======
-
->>>>>>> fa55f335617a561697966b07e9ceab36d017fc64
   ],
   exports: [BsDropdownModule, TooltipModule, ModalModule]
 })
