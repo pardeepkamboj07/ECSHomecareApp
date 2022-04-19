b35ca906428cf7b2748fe3c9630e969013dc45df
rakesh.ardanuy <rakesh.gangwar@ardanuy.com>
2022-04-19 17:17:18 +0530

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  domain : "https://localhost:44359",
-   // domain : "https://api.eshcs.com",
+  //domain : "https://localhost:44359",
+    domain : "https://api.eshcs.com",
    /* Invoice API URL */
    generateinvoiceurl : "/api/Invoice/generateinvoice",
    getinvoicelisturl : "/api/Invoice/getinvoicelist",
